9da6d5d31a5a028ed91ddd18ae0cb7af28b3daaa
Oliver Hayman <ollie.hayman@celaton.com>
2020-02-02 09:52:50 +0000

    Added order by

--- server/index.js
+++ server/index.js
@@ -40,7 +40,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * FROM values');
+    const values = await pgClient.query('SELECT * FROM values ORDER BY 1');
     
     res.send(values.rows);
 });
